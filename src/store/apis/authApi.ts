9c4ab6a5c1edb8a98b68a9f42b8f866ef589136d
TheJ8467 <121436038+TheJ8467@users.noreply.github.com>
2023-08-09 11:32:48 +0900

    updated baseUrl

--- src/store/apis/authApi.ts
+++ src/store/apis/authApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const authApi = createApi({
   reducerPath: 'auth',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://9d557s-3000.csb.app/',
+    baseUrl: 'https://9d557s-3005.csb.app/',
   }),
   tagTypes: ['User'],
   endpoints(builder) {
